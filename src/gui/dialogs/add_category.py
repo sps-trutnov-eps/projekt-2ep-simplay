293590e46424dea687eb398a4e898fcb323ee65e
Tomáš Klose <kloset22@spstrutnov.cz>
2023-09-21 19:59:26 +0200

    Přidán dialog na přidání kategorie

---
+++ src/gui/dialogs/add_category.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import *
+
+
+class AddCategoryDialog(QDialog):
+
+    def __init__(self, parent=None) -> None:
+        super(AddCategoryDialog, self).__init__(parent)
+
+        self.initUi()
+
+
+    def initUi(self):
+        pass
