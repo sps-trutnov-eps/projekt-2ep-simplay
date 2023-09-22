c2b160ea48d47875ead6df6989728e81fdea34b1
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-09-22 19:33:36 +0200

    Přidáno otevírací postranní menu

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -2,6 +2,11 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from gui.dialogs import *
 
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from Custom_Widgets.Widgets import QCustomSlideMenu
+
 
 class Ui_MainWindow(QMainWindow):
 
@@ -13,3 +18,107 @@ class Ui_MainWindow(QMainWindow):
         # TODO Here paste the design
         dialog = AddCategoryDialog()
         print("Setup ui...")
+
+        dialog = AddCategoryDialog()
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.header = QFrame(self.centralwidget)
+        self.header.setObjectName(u"header")
+        self.header.setMinimumSize(QSize(0, 50))
+        self.header.setMaximumSize(QSize(16777215, 50))
+        self.header.setFrameShape(QFrame.StyledPanel)
+        self.header.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.header)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.frame = QFrame(self.header)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 10, 75, 16))
+
+        self.horizontalLayout_2.addWidget(self.frame, 0, Qt.AlignLeft|Qt.AlignTop)
+
+        self.frame_3 = QFrame(self.header)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.label = QLabel(self.frame_3)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(140, 10, 47, 13))
+
+        self.horizontalLayout_2.addWidget(self.frame_3)
+
+
+        self.verticalLayout.addWidget(self.header)
+
+        self.frame_2 = QFrame(self.centralwidget)
+        self.frame_2.setObjectName(u"frame_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
+        self.frame_2.setSizePolicy(sizePolicy)
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame_2)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.side_menu = QCustomSlideMenu(self.frame_2)
+        self.side_menu.setObjectName(u"side_menu")
+        self.verticalLayout_2 = QVBoxLayout(self.side_menu)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.frame_4 = QFrame(self.side_menu)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.widget = QWidget(self.frame_4)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(130, 110, 77, 54))
+        self.verticalLayout_3 = QVBoxLayout(self.widget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.pushButton_2 = QPushButton(self.widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout_3.addWidget(self.pushButton_2, 0, Qt.AlignLeft|Qt.AlignTop)
+
+        self.pushButton_3 = QPushButton(self.widget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.verticalLayout_3.addWidget(self.pushButton_3)
+
+
+        self.verticalLayout_2.addWidget(self.frame_4)
+
+
+        self.horizontalLayout.addWidget(self.side_menu)
+
+        self.main_body = QFrame(self.frame_2)
+        self.main_body.setObjectName(u"main_body")
+        self.main_body.setFrameShape(QFrame.StyledPanel)
+        self.main_body.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.main_body)
+
+
+        self.verticalLayout.addWidget(self.frame_2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"MENU", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Simplay", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Odehran\u00fd \u010das", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"\u00da\u010det", None))
