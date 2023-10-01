6050aadb0ad0eef0fc33ba8b54258b5a9ef0e040
Tomáš Klose <kloset22@spstrutnov.cz>
2023-10-01 16:05:11 +0200

    Přidána veškerá tlačítka do hlavičky v dialogu

--- src/gui/dialogs/add_category.py
+++ src/gui/dialogs/add_category.py
@@ -19,12 +19,16 @@ class AddCategoryDialog(QDialog):
         header_layout = QVBoxLayout(header_widget)
         header_widget.setLayout(header_layout)
 
-        buttons_widget = QWidget(self)
+        buttons_widget = QWidget(self)                                                  # Buttons
         buttons_layout = QVBoxLayout(buttons_widget)
-        minimize_button = QPushButton(buttons_widget)
+        minimize_button = QPushButton(buttons_widget)                                       # Minimize button
         minimize_button.setIcon(QIcon("assets/icons/chevron-down.svg"))
         buttons_layout.addWidget(minimize_button)
-        restore_button = QPushButton(buttons_widget)
+        restore_button = QPushButton(buttons_widget)                                        # Restore button
         restore_button.setIcon(QIcon("assets/icons/maximize-2.svg"))
         buttons_layout.addWidget(restore_button)
+        close_button = QPushButton(buttons_widget)                                          # Close button
+        close_button.setIcon(QIcon("assets/icons/x.svg"))
+        buttons_layout.addWidget(close_button)
         buttons_widget.setLayout(buttons_layout)
+        
