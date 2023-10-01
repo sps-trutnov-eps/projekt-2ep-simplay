042ee46843658949124c18e0dd1140cc6d347b84
Tomáš Klose <kloset22@spstrutnov.cz>
2023-10-01 22:45:07 +0200

    Změna dialogu přidání kategorie

--- src/gui/dialogs/add_category.py
+++ src/gui/dialogs/add_category.py
@@ -2,33 +2,23 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+from gui.navigation_bar import NavigationBar
 
-class AddCategoryDialog(QDialog):
 
-    def __init__(self, parent=None) -> None:
-        super(AddCategoryDialog, self).__init__(parent)
+class AddCategoryDialog(QWidget):
+
+    def __init__(self) -> None:
+        super(AddCategoryDialog, self).__init__()
 
         self.setMinimumHeight(200)
         self.setMinimumWidth(300)
         self.initUi()
-        self.exec_()
+        self.show()
 
 
     def initUi(self):
-        header_widget = QWidget(self)
-        header_layout = QVBoxLayout(header_widget)
-        header_widget.setLayout(header_layout)
+        central_layout = QVBoxLayout(self)
+        central_layout.setContentsMargins(QMargins(0, 0, 0, 0))
 
-        buttons_widget = QWidget(self)                                                  # Buttons
-        buttons_layout = QVBoxLayout(buttons_widget)
-        minimize_button = QPushButton(buttons_widget)                                       # Minimize button
-        minimize_button.setIcon(QIcon("assets/icons/chevron-down.svg"))
-        buttons_layout.addWidget(minimize_button)
-        restore_button = QPushButton(buttons_widget)                                        # Restore button
-        restore_button.setIcon(QIcon("assets/icons/maximize-2.svg"))
-        buttons_layout.addWidget(restore_button)
-        close_button = QPushButton(buttons_widget)                                          # Close button
-        close_button.setIcon(QIcon("assets/icons/x.svg"))
-        buttons_layout.addWidget(close_button)
-        buttons_widget.setLayout(buttons_layout)
-        
+        navbar = NavigationBar(central_layout)
+        central_layout.addStretch(1)
