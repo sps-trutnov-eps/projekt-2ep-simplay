45d95eec77120d4c8b8d1801923bcea84fe5db28
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-10-04 16:17:55 +0200

    Přidána animace

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-
 from gui.views import *
 
 
@@ -9,10 +8,11 @@ class Ui_MainWindow(QMainWindow):
 
     def __init__(self) -> None:
         super(Ui_MainWindow, self).__init__()
-
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.setupHomeView()
-
-
+        loadJsonStyle(self, self.ui)
+        
     def setupHomeView(self):
         self.HomeView = HomeView(self)
         self.show()
