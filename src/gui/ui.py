9eeeb59994824cf145d12edf61ac2b6271c7a73e
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-10-08 13:33:45 +0200

    Animace se nechce spustit

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -2,17 +2,16 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 from gui.views import *
-
+from Custom_Widgets.Widgets import *
 
 class Ui_MainWindow(QMainWindow):
 
     def __init__(self) -> None:
         super(Ui_MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
         self.setupHomeView()
-        loadJsonStyle(self, self.ui)
+
         
     def setupHomeView(self):
         self.HomeView = HomeView(self)
+        loadJsonStyle(self, self.HomeView)
         self.show()
