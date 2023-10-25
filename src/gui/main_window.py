e6c21819da6b2706cde28093d8f1b206b1c22db7
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-10-25 19:33:42 +0200

    Přidány funkce pro vytvoření/smazání her/složek

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -14,3 +14,12 @@ class MainWindow(QMainWindow):
         })
 
         self.show()
+
+        self.insert.clicked.connect(self.insertTab)
+        self.remove.clicked.connect(self.removeTab)
+
+    def insertTab(self):
+        self.tabWidget.addTab(QWidget(), "Jméno hry")
+
+    def removeTab(self):
+        self.tabWidget.removeTab(0)
