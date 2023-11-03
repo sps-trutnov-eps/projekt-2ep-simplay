59155ae7044d1342edf130f3929266f5b51985dc
Tomáš Klose <kloset22@spstrutnov.cz>
2023-11-03 16:22:56 +0100

    Upravena adresářová struktura objektů

---
+++ src/Obj/game/__init__.py
@@ -0,0 +1,7 @@
+from obj.game.game import Game
+from obj.game.game_manager import GameManager
+
+__ALL__ = [
+    "Game",
+    "GameManager"
+]
--- src/Obj/game/game_manager.py
+++ src/Obj/game/game_manager.py
@@ -1,5 +1,4 @@
 from lxml import etree
-import xml
 
 from game import Game
 
