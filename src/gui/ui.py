54be153c6d622f0da79468efb26e8e2a6c8c5883
Tomáš Klose <kloset22@spstrutnov.cz>
2023-11-06 17:45:04 +0100

    Přidáno tlačítko pro spuštění hry

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -75,8 +75,13 @@ class Ui_MainWindow(object):
             self.subtitle.setStyleSheet(u"color: #28262C")
             self.layout.addWidget(self.subtitle)
 
+            self.button = QPushButton(self.game)
+            self.button.setText("Spustit")
+            self.button.setStyleSheet(u"height: 35; border: none; background-color: #C03E25; border-radius: 5; color: #FEFEFE;")
+            self.layout.addWidget(self.button)
+            self.button.clicked.connect(game.run)
+
             self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
             self.game.setLayout(self.layout)
-
 
             self.HomeView.games["game" + game.getUUID()] = self.game;
