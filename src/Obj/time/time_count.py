55b039cdb193685f537945093cb2ba944ece3d43
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-11-12 15:50:38 +0100

    Základní úprava třídy pro čas

--- src/Obj/time/time_count.py
+++ src/Obj/time/time_count.py
@@ -1,3 +1,15 @@
+import time
+
 class TimeCount():  
     def __init__(self):
-        pass
+        self.f = open('time.txt','r')
+
+    def time(self):
+        file = self.f.readlines()
+        last = int(file[0])
+
+        #time = 
+        self.f.close()
+        file = open('time.txt', 'w')
+        file.write(str(time))
+        file.close()  
