190118b7f07f3802a4fbcde8b5c77b3187a8fa7a
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-11-20 19:16:46 +0100

    Přidány defautní hry (nefunkční cesta)

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -28,11 +28,15 @@ class Ui_MainWindow(object):
         self.HomeView = HomeView()
         game_manager = GameManager()
 
-        game1 = Game()                                  # Adding default games to list
-        self.game_manager.games.append(game1)
+        # Adding default games to list
+        game1 = Game("0", "RPS", "data\rps.exe")                                  
+        game_manager.games.append(game1)
 
-        game2 = Game()
-        self.game_manager.games.append(game2)
+        game2 = Game("1", "TicTacToe", "data\tictactoe.exe")
+        game_manager.games.append(game2)
+
+        game3 = Game("2", "Hangman", "data\hangman.exe")
+        game_manager.games.append(game3)
 
         self.fillGamesWidget(game_manager)
 
@@ -113,35 +117,35 @@ class Ui_MainWindow(object):
             self.HomeView.games["game" + game.getUUID()] = self.game
 
         # Zde naházej hry do listu self.HomeView.games
-        #self.game = QWidget()
-        #self.game.setStyleSheet(u"background-color: #28262C")
-        #self.game.setMaximumWidth(100)
-        #self.game.setMinimumHeight(130)
-        #self.game.setContentsMargins(QMargins(5, 5, 5, 5))
-        #self.game.setCursor(QCursor(Qt.PointingHandCursor))
+        # self.game = QWidget()
+        # self.game.setStyleSheet(u"background-color: #28262C")
+        # self.game.setMaximumWidth(100)
+        # self.game.setMinimumHeight(130)
+        # self.game.setContentsMargins(QMargins(5, 5, 5, 5))
+        # self.game.setCursor(QCursor(Qt.PointingHandCursor))
 
-        #self.layout = QVBoxLayout(self.game)
-        #self.title = QLabel(self.game)
-        #self.title.setText(game.getName())
-        #self.title.setFont(self.gameFont)
-        #self.title.setStyleSheet(u"color: #FEFEFE")
-        #self.layout.addWidget(self.title)
-        #self.subtitle = QLabel(self.game)
-        #self.subtitle.setText(game.getUUID())
-        #self.subtitle.setFont(self.gameSubFont)
-        #self.subtitle.setStyleSheet(u"color: #28262C")
-        #self.layout.addWidget(self.subtitle)
+        # self.layout = QVBoxLayout(self.game)
+        # self.title = QLabel(self.game)
+        # self.title.setText(game.getName())
+        # self.title.setFont(self.gameFont)
+        # self.title.setStyleSheet(u"color: #FEFEFE")
+        # self.layout.addWidget(self.title)
+        # self.subtitle = QLabel(self.game)
+        # self.subtitle.setText(game.getUUID())
+        # self.subtitle.setFont(self.gameSubFont)
+        # self.subtitle.setStyleSheet(u"color: #28262C")
+        # self.layout.addWidget(self.subtitle)
 
-        #self.button = QPushButton(self.game)
-        #self.button.setText("Spustit")
-        #self.button.setStyleSheet(u"height: 35; border: none; background-color: #C03E25; border-radius: 5; color: #FEFEFE;")
-        #self.layout.addWidget(self.button)
-        #self.button.clicked.connect(game.run)
+        # self.button = QPushButton(self.game)
+        # self.button.setText("Spustit")
+        # self.button.setStyleSheet(u"height: 35; border: none; background-color: #C03E25; border-radius: 5; color: #FEFEFE;")
+        # self.layout.addWidget(self.button)
+        # self.button.clicked.connect(game.run)
 
-        #self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        #self.game.setLayout(self.layout)
+        # self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # self.game.setLayout(self.layout)
 
-        #self.HomeView.games["game" + game.getUUID()] = self.game
+        # self.HomeView.games["game" + game.getUUID()] = self.game
         # Atd.
 
     def setPassword(self, password: str) -> None:
