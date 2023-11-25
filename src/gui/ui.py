b6f0aa9c1c84b489662d8f20086bb12605393559
hajnysl22 <hajnysl22@spstrutnov.cz>
2023-11-25 12:37:41 +0100

    Odebráno nepotřebné tlačítko

--- src/gui/ui.py
+++ src/gui/ui.py
@@ -129,37 +129,48 @@ class Ui_MainWindow(object):
 
             self.HomeView.games["game" + game.getUUID()] = self.game
 
-        
-        # self.category = QWidget()
-        # self.category.setStyleSheet(u"background-color: #28262C")
-        # self.category.setMaximumWidth(100)
-        # self.category.setMinimumHeight(130)
-        # self.category.setContentsMargins(QMargins(5, 5, 5, 5))
-        # self.category.setCursor(QCursor(Qt.PointingHandCursor))
 
-        # self.layout = QVBoxLayout(self.category)
-        # self.title = QLabel(self.category)
-        # self.title.setText(game.getName())
-        # self.title.setFont(self.categoryFont)
-        # self.title.setStyleSheet(u"color: #FEFEFE")
-        # self.layout.addWidget(self.title)
-        # self.subtitle = QLabel(self.category)
-        # self.subtitle.setText(game.getUUID())
-        # self.subtitle.setFont(self.categorySubFont)
-        # self.subtitle.setStyleSheet(u"color: #28262C")
-        # self.layout.addWidget(self.subtitle)
+    def fillCategoriesWidget(self, add_category: CategoryManager) -> None:
+        self.categoryFont = QFont()
+        self.categoryFont.setFamily(u"Segoe UI")
+        self.categoryFont.setPointSize(12)
 
-        # self.button = QPushButton(self.category)
-        # self.button.setText("Spustit")
-        # self.button.setStyleSheet(u"height: 35; border: none; background-color: #C03E25; border-radius: 5; color: #FEFEFE;")
-        # self.layout.addWidget(self.button)
-        # self.button.clicked.connect(game.run)
+        self.categorySubFont = QFont()
+        self.categorySubFont.setFamily(u"Segoe UI")
+        self.categorySubFont.setPointSize(10)
 
-        # self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        # self.category.setLayout(self.layout)
+        for category in add_category.getCategories():
+            self.category = QWidget()
+            self.category.setStyleSheet(u"background-color: #28262C")
+            self.category.setMaximumWidth(100)
+            self.category.setMinimumHeight(130)
+            self.category.setContentsMargins(QMargins(5, 5, 5, 5))
+            self.category.setCursor(QCursor(Qt.PointingHandCursor))
 
-        # self.HomeView.games["game" + game.getUUID()] = self.category
-        # Atd.
+            self.layout = QVBoxLayout(self.category)
+            self.title = QLabel(self.category)
+            self.title.setText(category.getName())
+            self.title.setFont(self.categoryFont)
+            self.title.setStyleSheet(u"color: #FEFEFE")
+            self.layout.addWidget(self.title)
+            self.subtitle = QLabel(self.category)
+            self.subtitle.setText(category.getUUID())
+            self.subtitle.setFont(self.categorySubFont)
+            self.subtitle.setStyleSheet(u"color: #28262C")
+            self.layout.addWidget(self.subtitle)
+
+            self.button = QPushButton(self.category)
+            self.button.setText("Zobrazit")
+            self.button.setStyleSheet(u"height: 35; border: none; background-color: #C03E25; border-radius: 5; color: #FEFEFE;")
+            self.layout.addWidget(self.button)
+
+            #self.button.clicked.connect(game.run)
+
+            self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.category.setLayout(self.layout)
+
+            # self.HomeView.games["game" + game.getUUID()] = self.category
+            # Atd.
         
 
 
